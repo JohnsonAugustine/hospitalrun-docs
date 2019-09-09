087a6f222a66992d9dccdf9d3a43310864824d21
Michael J Feher <github@phearzero.com>
2019-09-09 08:38:36 -0500

    :pencil2: (storybook-components) Fixed refrence to README

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react';
 import ReadmePage from '../pages/ReadmePage'
 
 storiesOf('@hospitalrun-org/docs', module)
-    .add('ReadmePage', () => <ReadmePage markdown={require("../templates/Home.md")}/>)
+    .add('ReadmePage', () => <ReadmePage markdown={require("../README.md")}/>)
